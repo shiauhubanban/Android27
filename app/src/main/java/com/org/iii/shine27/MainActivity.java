32c46fb69255627f6300c442cafba04f8c57af2f
user <a>
2016-12-01 11:52:26 +0800

    v3 非同步

--- app/src/main/java/com/org/iii/shine27/MainActivity.java
+++ app/src/main/java/com/org/iii/shine27/MainActivity.java
@@ -3,6 +3,7 @@ package com.org.iii.shine27;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void test1(View v){
-
+        MyAsyncTask myTask = new MyAsyncTask();
+        myTask.execute();
     }
 
 
@@ -22,26 +24,31 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            Log.v("brad", "onPreExecute");
+        }
+        @Override
+        protected Void doInBackground(Void... nulls) {
+            Log.v("brad", "doInBackground");
+            return null;
         }
 
         @Override
         protected void onPostExecute(Void aNull) {
             super.onPostExecute(aNull);
+            Log.v("brad", "onPostExecute");
         }
 
         @Override
         protected void onProgressUpdate(Void... values) {
             super.onProgressUpdate(values);
+            Log.v("brad", "onProgressUpdate");
         }
 
         @Override
         protected void onCancelled(Void aNull) {
             super.onCancelled(aNull);
+            Log.v("brad", "onCancelled");
         }
 
-        @Override
-        protected Void doInBackground(Void... nulls) {
-            return null;
-        }
     }
 }
