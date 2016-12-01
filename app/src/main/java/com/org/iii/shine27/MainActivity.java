f8381289edda78a984f1204a704e081cd247a974
user <a>
2016-12-01 12:03:13 +0800

    v4 非同步

--- app/src/main/java/com/org/iii/shine27/MainActivity.java
+++ app/src/main/java/com/org/iii/shine27/MainActivity.java
@@ -16,19 +16,24 @@ public class MainActivity extends AppCompatActivity {
 
     public void test1(View v){
         MyAsyncTask myTask = new MyAsyncTask();
-        myTask.execute();
+        myTask.execute("Brad","Tony","Eric","Mary","Peter");
     }
 
-
-    private class MyAsyncTask extends AsyncTask<Void,Void,Void>{
+    
+    private class MyAsyncTask extends AsyncTask<String,Void,Void>{
+        //背景就做了
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
             Log.v("brad", "onPreExecute");
         }
+
+        //丟資料進去
         @Override
-        protected Void doInBackground(Void... nulls) {
-            Log.v("brad", "doInBackground");
+        protected Void doInBackground(String... names) {
+            for (String name : names) {
+                Log.v("brad", "doInBackground:" + name);
+            }
             return null;
         }
 
