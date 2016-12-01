bac2ccab1d513ad641d182a9f7b43f33ac9dbd28
user <a>
2016-12-01 12:18:23 +0800

    v5 非同步AsyncTask

--- app/src/main/java/com/org/iii/shine27/MainActivity.java
+++ app/src/main/java/com/org/iii/shine27/MainActivity.java
@@ -5,13 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mesg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mesg = (TextView)findViewById(R.id.mesg);
     }
 
     public void test1(View v){
@@ -19,22 +22,34 @@ public class MainActivity extends AppCompatActivity {
         myTask.execute("Brad","Tony","Eric","Mary","Peter");
     }
 
-    
-    private class MyAsyncTask extends AsyncTask<String,Void,Void>{
+
+    private class MyAsyncTask extends AsyncTask<String,Integer,Void>{
         //背景就做了
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
             Log.v("brad", "onPreExecute");
         }
-
         //丟資料進去
         @Override
         protected Void doInBackground(String... names) {
+            int i = 0;
             for (String name : names) {
                 Log.v("brad", "doInBackground:" + name);
+                publishProgress(i, i+10, i+100);
+                try {
+                    Thread.sleep(500);
+                }catch (Exception e){}
+                i++;
             }
             return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            mesg.setText("onProgressUpdate:" + values[0] + ":" +
+                    values[1] + ":" + values[2]);
         }
 
         @Override
@@ -43,11 +58,6 @@ public class MainActivity extends AppCompatActivity {
             Log.v("brad", "onPostExecute");
         }
 
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-            Log.v("brad", "onProgressUpdate");
-        }
 
         @Override
         protected void onCancelled(Void aNull) {
