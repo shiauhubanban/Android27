14ba81282988b0ead1d553992b2e4ba8106f5fd0
user <a>
2016-12-01 11:45:18 +0800

    v2

--- app/src/main/java/com/org/iii/shine27/MainActivity.java
+++ app/src/main/java/com/org/iii/shine27/MainActivity.java
@@ -1,7 +1,9 @@
 package com.org.iii.shine27;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void test1(View v){
+
+    }
+
+
+    private class MyAsyncTask extends AsyncTask<Void,Void,Void>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void aNull) {
+            super.onPostExecute(aNull);
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onCancelled(Void aNull) {
+            super.onCancelled(aNull);
+        }
+
+        @Override
+        protected Void doInBackground(Void... nulls) {
+            return null;
+        }
     }
 }
